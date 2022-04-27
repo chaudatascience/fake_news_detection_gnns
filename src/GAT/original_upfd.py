dd4ec6d265e09ff0a28f35922c84a9e42e614482
Efe Şencan <31569634+Efesencan@users.noreply.github.com>
2022-04-27 12:18:18 -0400

    Experimented GAT with different datasets & log results

--- src/GAT/original_upfd.py
+++ src/GAT/original_upfd.py
@@ -9,18 +9,17 @@ from torch_geometric.datasets import UPFD
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GATConv, GCNConv, SAGEConv, global_max_pool
 from torch_geometric.transforms import ToUndirected
-import pandas as pd
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='gossipcop',
+parser.add_argument('--dataset', type=str, default='politifact',
                     choices=['politifact', 'gossipcop'])
-parser.add_argument('--feature', type=str, default='bert',
+parser.add_argument('--feature', type=str, default='spacy',
                     choices=['profile', 'spacy', 'bert', 'content'])
-parser.add_argument('--model', type=str, default='GAT',
+parser.add_argument('--model', type=str, default='GCN',
                     choices=['GCN', 'GAT', 'SAGE'])
 args = parser.parse_args()
 
-#path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'UPFD')
-path = './'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'UPFD')
 train_dataset = UPFD(path, args.dataset, args.feature, 'train', ToUndirected())
 val_dataset = UPFD(path, args.dataset, args.feature, 'val', ToUndirected())
 test_dataset = UPFD(path, args.dataset, args.feature, 'test', ToUndirected())
@@ -101,30 +100,11 @@ def test(loader):
 
     return total_correct / total_examples
 
-@torch.no_grad()
-def test_loss_function(passed_data):
-    model.eval()
-    total_loss = 0
-    for data in passed_data:
-        data = data.to(device)
-        optimizer.zero_grad()
-        out = model(data.x, data.edge_index, data.batch)
-        loss = F.nll_loss(out, data.y)
-        #loss.backward() # this may not be required
-        optimizer.step()
-        total_loss += float(loss) * data.num_graphs
 
-    return total_loss / len(passed_data.dataset)
-
-record_metrics = pd.DataFrame(columns = ['Epoch', 'train_acc', 'train_loss', 'val_acc','test_acc'])
-for epoch in range(1, 201):
+for epoch in range(1, 501):
     loss = train()
     train_acc = test(train_loader)
     val_acc = test(val_loader)
     test_acc = test(test_loader)
-    val_loss, test_loss = test_loss_function(val_loader), test_loss_function(test_loader)
-    record_metrics = record_metrics.append({'Epoch': f'{epoch:03d}', 'train_acc': f'{train_acc:.4f}', 'train_loss': f'{loss:.4f}','val_acc': f'{val_acc:.4f}', 'val_loss':f'{val_loss:.4f}','test_acc': f'{test_acc:.4f}','test_loss': f'{test_loss:.4f}'}, ignore_index=True)
-    print(f'Epoch: {epoch:03d}, train_acc: {train_acc:.4f}, train_loss: {loss:.4f},val_acc: {val_acc:.4f}, val_loss: {val_loss:.4f}, test_acc: {test_acc:.4f}, test_loss: {test_loss:.4f}'
-          f'test_acc: {test_acc:.4f}')
-
-record_metrics.to_csv('gossipcop_bert_GAT_results.csv', index=False)
+    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
+          f'Val: {val_acc:.4f}, Test: {test_acc:.4f}')
